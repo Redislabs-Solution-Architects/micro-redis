ac980fecefbdd49b4d6a57c218c1ca052d1ffb55
Eric Preston <eric.preston@redis.com>
2023-06-27 08:49:08 -0500

    cleanup test

--- newuser/tests/test_new_user.py
+++ newuser/tests/test_new_user.py
@@ -1,5 +1,6 @@
 import os
 import time
+import unittest
 
 from app.new_user_service import NewUserService
 from app.redis_connection import RedisConnection
@@ -11,25 +12,25 @@ load_dotenv()
 STREAM_NAME = os.getenv("REDIS_USER_STREAM", "new_user")
 
 
-def test_new_user_no_stream():
-    results = NewUserService().read_stream("0_0")
-    assert results is None
+class TestNewUser(unittest.TestCase):
+    def test_new_user_no_stream(self):
+        results = NewUserService().read_stream("0_0")
+        self.assertIsNone(results)
 
+    def test_new_user_with_stream(self):
+        redis = RedisConnection.get_client()
 
-def test_new_user_with_stream():
-    redis = RedisConnection.get_client()
+        user = dict(
+            first_name="test_first",
+            last_name="test_last",
+            email="test_mail@example.com")
 
-    user = dict(
-        first_name="test_first",
-        last_name="test_last",
-        email="test_mail@example.com")
+        redis.xadd(STREAM_NAME, user)
 
-    redis.xadd(STREAM_NAME, user)
+        print("sleeping")
+        time.sleep(2)
+        print("awake")
 
-    print("sleeping")
-    time.sleep(2)
-    print("awake")
-
-    results = NewUserService().read_stream("0-0")
-    entry = results[0][1][0]
-    assert entry[1]["first_name"] == "test_first"
+        results = NewUserService().read_stream("0-0")
+        entry = results[0][1][0]
+        self.assertEqual("test_first", entry[1]["first_name"])
